c41aa4934589b879c68a1a72a8dc9aebec044393
thiago-rigonatti <thiago_rigonatti@outlook.com.br>
2021-12-06 17:49:42 -0300

    1.18 update

--- src/org/black_ixx/bossshop/managers/config/ConfigLoader.java
+++ src/org/black_ixx/bossshop/managers/config/ConfigLoader.java
@@ -1,6 +1,5 @@
 package org.black_ixx.bossshop.managers.config;
 
-import org.apache.commons.lang.Validate;
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.YamlConfiguration;
@@ -13,7 +12,12 @@ import java.util.logging.Level;
 public class ConfigLoader {
 
     public static YamlConfiguration loadConfiguration(File file, boolean debug) throws InvalidConfigurationException {
-        Validate.notNull(file, "File cannot be null");
+
+        if(Bukkit.getVersion().equals("1.18")){
+            org.apache.commons.lang3.Validate.notNull(file, "File cannot be null");
+        }else{
+            org.apache.commons.lang.Validate.notNull(file, "File cannot be null");
+        }
 
         YamlConfiguration config = new YamlConfiguration();
 
